7d81ab7c7dea20744cd4849b4984bc5128760df4
pankajkdev <pankajk.dev0000@gmail.com>
2024-07-27 22:34:41 +0530

    Setup and configuration complete

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,1 +1,11 @@
 import multer from "multer";
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "../public/temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "_" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
+  },
+});
+export const upload = multer({ storage });
