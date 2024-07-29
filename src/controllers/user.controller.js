6c0abfd0904daff85cde8cbf48d145f5c3cf6a8b
pankajkdev <pankajk.dev0000@gmail.com>
2024-07-29 13:17:42 +0530

    added functionality to register users,validation and configured cloudinary with multer

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 const registerUser = asyncHandler(async (req, res) => {
   //get user details from frontend
   //validation - not empty
@@ -11,8 +14,67 @@ const registerUser = asyncHandler(async (req, res) => {
   //check for user creation
   //return response
 
-  const { fullname, email, username, password } = req.body;
+  const { fullName, email, username, password } = req.body;
   console.log(email, password);
+  //if any of the field is true some will return true
+  //some checks if at least one element passes the test implemented in callback
+  if (
+    [fullName, email, username, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are compulsory");
+  }
+  if (!email.includes("@")) {
+    throw new ApiError(400, "Please add a valid email address");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
+    //checks for username and email in database to match
+  });
+  if (existedUser) {
+    throw new ApiError(409, "User already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "avatar is required");
+  }
+
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+  if (!avatar) {
+    throw new ApiError(400, "avatar is required");
+  }
+  //creating user entries
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.toLowerCase(),
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    //by default all values are selected
+    "-password -refreshToken"
+    //minus - sign means to remove these from the User object
+    // password and refreshToken will be removed
+    //
+  );
+
+  if (!createdUser) {
+    throw new ApiError(
+      500,
+      "Something went wrong while creating user...Please try later"
+    );
+  }
+
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "User Registered Successfully"));
 });
 
 export { registerUser };
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,11 +53,11 @@ const userSchema = new Schema(
 );
 //dont use arrow because of this
 //its a middleware so needs next
-userSchema.pre("save", function (next) {
+userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     return next();
   }
-  this.password = bcrypt.hash(this.password, 10);
+  this.password = await bcrypt.hash(this.password, 10);
   next();
 });
 userSchema.methods.isPasswordCorrect = async function (password) {
